e46843e40ab4d94364375a5b86fc8c193bef3750
willian emboava <w.emboava.o@gmail.com>
2020-10-12 22:57:27 -0300

    add apollo server express

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,36 @@
-import { ApolloServer } from 'apollo-server';
-
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-var-requires */
 import * as resolvers from './graphql/resolvers/index';
 import * as schemas from './graphql/schemas/index';
 
-console.log(Object.values({ ...resolvers }));
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
 
 const server = new ApolloServer({
   typeDefs: Object.values(schemas),
   resolvers: Object.values({ ...resolvers }),
 });
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
+
+// import { ApolloServer } from 'apollo-server';
+
+// import * as resolvers from './graphql/resolvers/index';
+// import * as schemas from './graphql/schemas/index';
+
+// console.log(Object.values({ ...resolvers }));
+
+// const server = new ApolloServer({
+//   typeDefs: Object.values(schemas),
+//   resolvers: Object.values({ ...resolvers }),
+// });
+
+// // eslint-disable-next-line @typescript-eslint/no-floating-promises
+// server.listen().then(({ url }) => {
+//   console.log(`🚀 Server ready at ${url}`);
+// });
