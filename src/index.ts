a36c0c31870d2aeeab3814c95c4226beee017cea
willian emboava <w.emboava.o@gmail.com>
2020-12-13 01:55:20 -0300

    add playground settings

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,23 @@
-import { ApolloServer } from 'apollo-server-express';
-// import cors from 'cors';
-import express from 'express';
-import { importSchema } from 'graphql-import';
+import { ApolloServer } from 'apollo-server';
 
 import * as resolvers from './graphql/resolvers/index';
 import * as schemas from './graphql/schemas/index';
 
+console.log(Object.values({ ...resolvers }));
+
 const server = new ApolloServer({
-  typeDefs: importSchema(schemas),
-  resolvers,
-  playground: true,
+  typeDefs: Object.values(schemas),
+  resolvers: Object.values({ ...resolvers }),
+  playground: {
+    settings: {
+      // So that auth works
+      // Docs: https://github.com/prisma/graphql-playground
+      ['request.credentials']: 'same-origin',
+    },
+  },
 });
 
-const app = express();
-
-// app.use(cors(corsOptions));
-
-server.applyMiddleware({ app });
-
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
-);
-
-// import { ApolloServer } from 'apollo-server';
-
-// import * as resolvers from './graphql/resolvers/index';
-// import * as schemas from './graphql/schemas/index';
-
-// console.log(Object.values({ ...resolvers }));
-
-// const server = new ApolloServer({
-//   typeDefs: Object.values(schemas),
-//   resolvers: Object.values({ ...resolvers }),
-// playground: true,
-// cors: true,
-// });
-
-// // eslint-disable-next-line @typescript-eslint/no-floating-promises
-// server.listen().then(({ url }) => {
-//   console.log(`🚀 Server ready at ${url}`);
-// });
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
