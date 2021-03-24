8fee2be5a6e8433d1a40a12b3fc41bf6499be6cc
willian emboava <w.emboava.o@gmail.com>
2021-03-24 12:51:21 -0300

    change server test

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,50 @@
-import { ApolloServer } from 'apollo-server-express';
-import cors from 'cors';
-import express from 'express';
-import { importSchema } from 'graphql-import';
-
-import * as resolvers from './graphql/resolvers/index';
-import * as schemas from './graphql/schemas/index';
-
-const server = new ApolloServer({
-  typeDefs: importSchema(schemas),
-  resolvers,
-  introspection: true,
-  playground: {
-    settings: {
-      // So that auth works
-      // Docs: https://github.com/prisma/graphql-playground
-      ['request.credentials']: 'same-origin',
-    },
-  },
-  subscriptions: { path: '/api' },
-});
-
-const app = express();
-
-app.use(cors());
-
-server.applyMiddleware({ app });
-
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
-);
-
-// import { ApolloServer } from 'apollo-server';
+// import { ApolloServer } from 'apollo-server-express';
+// import cors from 'cors';
+// import express from 'express';
+// import { importSchema } from 'graphql-import';
 
 // import * as resolvers from './graphql/resolvers/index';
 // import * as schemas from './graphql/schemas/index';
 
-// console.log(Object.values({ ...resolvers }));
-
 // const server = new ApolloServer({
-//   typeDefs: Object.values(schemas),
-//   resolvers: Object.values({ ...resolvers }),
-// playground: true,
-// cors: true,
+//   typeDefs: importSchema(schemas),
+//   resolvers,
+//   introspection: true,
+//   playground: {
+//     settings: {
+//       // So that auth works
+//       // Docs: https://github.com/prisma/graphql-playground
+//       ['request.credentials']: 'same-origin',
+//     },
+//   },
+//   subscriptions: { path: '/api' },
 // });
 
-// // eslint-disable-next-line @typescript-eslint/no-floating-promises
-// server.listen().then(({ url }) => {
-//   console.log(`🚀 Server ready at ${url}`);
-// });
+// const app = express();
+
+// app.use(cors());
+
+// server.applyMiddleware({ app });
+
+// app.listen({ port: 4000 }, () =>
+//   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+// );
+
+import { ApolloServer } from 'apollo-server';
+
+import * as resolvers from './graphql/resolvers/index';
+import * as schemas from './graphql/schemas/index';
+
+console.log(Object.values({ ...resolvers }));
+
+const server = new ApolloServer({
+  typeDefs: Object.values(schemas),
+  resolvers: Object.values({ ...resolvers }),
+  playground: true,
+  cors: true,
+});
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
